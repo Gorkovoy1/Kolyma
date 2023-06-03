cd0f7d4b712c9f01a741de6f9a13455d1488644e
Gorkovoy1 <101889868+Gorkovoy1@users.noreply.github.com>
2023-06-03 16:49:45 -0700

    Update Dialogue New
    Edited the text

--- Assets/Scripts/Dialogue.cs
+++ Assets/Scripts/Dialogue.cs
@@ -50,35 +50,35 @@ public class Dialogue : MonoBehaviour
 
 
 
-    lines = new List<string>() {"Don't worry, I know the rules. You were here first and the bed is yours. But can I still sit here? It has been a long and tiring journey.",
+    lines = new List<string>() {"Old Man: You don't look like a thief. So, can I ask you a question? Oh, don't worry, I know the rules. You were here first and the bed is yours. But can I still sit here?.. I never ask for anything, but you see... It's been a long and tiring journey.",
         "",
-        "It's just unthinkable! A week on a train, two days on a boat and here we are.",
-        "I have been to this part of Russia before, but never could I imagine I'd have to travel all the way to Magadan again.",
-        "My last name is Andreev, my first name is Aleksander.",
+        "Old Man: It's just unthinkable! A week on a train, two days on a boat and here we are.",
+        "Old Man: I've been to this part of Russia before, but never could I imagine I'd have to travel all the way to Magadan again.",
+        "Old Man: I am Andreyev, Alexander Andreyev.",
         "",
-        "It is quite ironic. I was arrested for anti-government activity in 1905 and I am an enemy of the Soviet state today. Things did not change for me that much.",
-        "Our Revolution was a dream that never came true. Well, you are here, there's no need to tell you that. What is your name?",
+        "Andreyev: It is quite ironic. I was arrested for anti-government activity in 1905 and I am an enemy of the Soviet state today, in 1938. Things did not change for me that much!",
+        "Andreyev: Our Revolution was a dream that never came true. Well, you are sharing this cell with me, there's no need to tell you that... What is your name?",
         "",
         "",
-        "I am sorry. Let me give you some advice. Put your former self to slumber. Focus on what is important now.",
-        "Try to kill the time and not to kill yourself in the process. You can trust me on this one, I know what I am talking about.",
-        "Let's play a simple game. It is very popular around here so you better remember the rules. It might even save your life one day so listen carefully."
+        "Andreyev: I am sorry... But let me give you some advice. Forget about your former self. Focus on more important matters.",
+        "Andreyev: You are a prisoner now. Remember, time is your main enemy. You can trust me on this one, I know what I am talking about.",
+        "Andreyev: To kill the time, let's play a simple game. It's called Numbers. It is very popular among the prisoners so you better remember the rules. It might even save your life one day so listen carefully"
         };
 
 
 
         indexList = new List<int>() {1, 5, 8, 9};
 
-        outcome1 = new List<string>() {"Oh, I am so grateful.", 
-        "Yes I was. A member since 1901.", "If you don't mind me asking, was General Kojukh your father?", "His trial was a joke, and you could tell how badly those miserable party clerks wanted to get rid of him."};
+        outcome1 = new List<string>() {"Old Man: Oh, I am so grateful!", 
+        "Andreyev: Yes I was. A member since 1901.", "Andreyev: If you don't mind me asking, was General Kojukh your father?", "Andreyev: His trial was a joke, and you could tell how badly those miserable party clerks wanted to get rid of him."};
 
 
-        outcome2 = new List<string>() {"Rules are rules. Although I really doubt anyone will be able to catch some sleep in such a packed cell.", 
-        "I did. But they lured me into a trap. I have no regrets though, I'd rather die here than in France, like one of those impotent immigrants.", "As you wish. As far as I have heard, someone here called you Arkady. That's good enough for me.", ""};
+        outcome2 = new List<string>() {"Old Man: Rules are rules. Although I really doubt anyone will be able to catch some sleep in such a packed cell.", 
+        "Andreyev: I did. But they lured me into a trap. I have no regrets though, I'd rather die here than in France, like one of those impotent pathetic immigrants.", "As you wish. As far as I have heard, someone here called you Arkady. That's good enough for me.", ""};
         
         
-        outcome3 = new List<string>() {"Your heart is soft. Don't worry, it will not stay that way for too long.",
-        "It sure was. But still, you should be careful who you trust.", "", ""};
+        outcome3 = new List<string>() {"Old Man: Hm. I thank you. Your heart is soft. Don't worry, it won't stay that way for too long",
+        "Andreyev: It sure was. They are calling everyone a spy now. They need us, the scapegoats, for the Purge to happen. Ha! One day they will pay for this!", "", ""};
 
 
         textComponent.text = string.Empty;
@@ -161,13 +161,13 @@ public class Dialogue : MonoBehaviour
         
         if(textComponent.text == lines[index] && index == 0)
         {
-            DisplayChoices("Sure. You may sit down.", "You may sit down. But do not forget you're sleeping on the floor tonight.", "Of course. Sit down here, it's hard to stand in such a crowded room.");
+            DisplayChoices("Sure. You may sit down.", "You may sit down. But do not forget you're sleeping on the floor tonight.", "Of course. Sit down here, it's hard to stand in such a crowded cell.");
             yield return null;
         }
 
         if(textComponent.text == lines[index] && index == 4)
         {
-            DisplayChoices("I know you. You were a member of the Socialist Revolutionary Party.", "Former terrorist? I thought you left the country.", "I heard you were a spy. I am sure it was a lie.");
+            DisplayChoices("I know you. You were a member of the Socialist Revolutionary Party.", "Former terrorist? I thought you left the country.", "I heard you were a spy. Was it another false accusation?");
             yield return null;
         }
 
