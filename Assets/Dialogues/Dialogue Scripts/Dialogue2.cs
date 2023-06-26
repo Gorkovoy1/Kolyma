680a956b4471d456abfc8da43c35da5faec96474
jazzydingo <129543893+jazzydingo@users.noreply.github.com>
2023-06-25 23:53:44 -0700

    Fade out and Dialogue 2 fixes
    fixed what we wanted to fix lol :)

--- Assets/Dialogues/Dialogue Scripts/Dialogue2.cs
+++ Assets/Dialogues/Dialogue Scripts/Dialogue2.cs
@@ -37,7 +37,16 @@ public class Dialogue2 : MonoBehaviour
 
     public Image NPC;
     public Image arkady;
-    public Image canvas;
+    public Image panel;
+    public Image nameTagPanel;
+
+    public Color panelColor;
+
+    public bool isItalic;
+    public bool endOfString;
+    public bool isRed;
+
+    public TMP_FontAsset numbersFont;
 
     [SerializeField] private ButtonScript choiceList;
 
@@ -48,19 +57,20 @@ public class Dialogue2 : MonoBehaviour
         choiceNumber = 0;
         choice = false;
         choiceList.gameObject.SetActive(false);
-
+        panelColor = panel.GetComponent<Image>().color;
 
         //<i>italicized</i>
         //<u> underline </i>
 
 
-        lines = new List<string>() {"Well, now you know how Numbers work",
-        "Take this deck, I have a spare one. Keep it close to your heart",
+        lines = new List<string>() {"Well, now you know how Numbers work.",
+        "Take this deck, I have a spare one. Keep it close to your heart.",
         "Now if you don't mind, I want to close my eyes for a couple of minutes...",
         "",
+        "",
         "Andreyev. Andreyev, show yourself!",
-        "They are taking me away. It is my turn now. Time to go",
-        "Accept this as a token of my gratitude for letting me spend some time on this bed",
+        "They are taking me away. It is my turn now. Time to go.",
+        "Accept this as a token of my gratitude for letting me spend some time on this bed.",
         "Here. Give it to someone who tried to make the world a better place, but failed. Just like me...",
        
         };
@@ -86,7 +96,7 @@ public class Dialogue2 : MonoBehaviour
 
         if (Input.GetMouseButtonDown(0))
         {
-            if (textComponent.text == lines[index] && !choice)
+            if (endOfString && !choice && index != 3)
             {
                 NextLine();
             }
@@ -111,35 +121,49 @@ public class Dialogue2 : MonoBehaviour
         
          
 
-        if (index==4) 
+        if (index==5) 
         {
             nameTag.text = "Convoy Officer";
             textComponent.font = italics;
-        }
-        else { 
-            
-            nameTag.text = "Andreyev";
-            textComponent.font = regular;
-        
-        }
-        if (index == 3)
-        {
             NPC.gameObject.SetActive(false);
             arkady.gameObject.SetActive(false);
-            textComponent.gameObject.SetActive(false);
-            nameTag.gameObject.SetActive(false);
-            Color colorCanvas = canvas.color;
-            colorCanvas = new Color(colorCanvas.r, colorCanvas.g, colorCanvas.b, 0f);
+            Color opaquePanel = new Color(panelColor.r, panelColor.g, panelColor.b, 1f);
+            panel.GetComponent<Image>().color = opaquePanel;
+            nameTagPanel.GetComponent<Image>().color = opaquePanel;
         }
-        else
-        {
+        else if (index != 3) 
+        { 
             NPC.gameObject.SetActive(true);
             arkady.gameObject.SetActive(true);
-            textComponent.gameObject.SetActive(true);
-            nameTag.gameObject.SetActive(true);
-            
+            nameTag.text = "Andreyev";
+            textComponent.font = regular;
         }
-    }   
+
+
+        
+    }  
+    
+    IEnumerator FadeToBlack(Image panel)
+    {
+        float fadeDuration = 1f; // Duration of the fade effect in seconds
+        float elapsedTime = 0f;
+
+        while (elapsedTime < fadeDuration)
+        {
+            float alpha = Mathf.Lerp(1f, 0f, elapsedTime / fadeDuration);
+            panelColor.a = alpha; // Update the alpha value of the original panelColor
+            panel.GetComponent<Image>().color = panelColor;
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+
+        yield return new WaitForSeconds(1.8f);
+
+        panelColor.a = 1f; // Set alpha value back to 1 (fully opaque)
+        panel.GetComponent<Image>().color = panelColor;
+        NextLine();
+        // Once the fade is complete, you can perform any desired actions
+    }
 
     void HighlightNPC()
     {
@@ -170,19 +194,67 @@ public class Dialogue2 : MonoBehaviour
 
     IEnumerator TypeLine()
     {
-        if (index == 3)
-        {
-            yield return new WaitForSeconds(2);
-        }
-
+   
         textSpeed = 0.03f;
         choice = false;
-        foreach (char c in lines[index].ToCharArray())
+        endOfString = false;
+        char[] charArray = lines[index].ToCharArray();
+        for(int i = 0; i < charArray.Length; i++)
         {
-            textComponent.text += c;
-            yield return new WaitForSeconds(textSpeed);
-        }
+            char c = charArray[i];
+            if(c == ' ')
+            {
+                isItalic = false;
+                isRed = false;
+            }
+            if (c == 'N')
+            {
+                isRed = true;            
+            }
+            if(c == '<')
+            {
+                isItalic = true;
+                continue;
+            }
 
+            if(isItalic)
+            {
+                textComponent.text += "<i>" + c + "</i>";
+            }
+            else if(isRed)
+            {
+                //textComponent.text += "<color=blue>" + c + "</color>";
+                textComponent.text += "<font=\"" + numbersFont.name + "\">" + c + "</font>";
+                
+            }
+            else
+            {
+                textComponent.text += c;
+            }
+
+            yield return new WaitForSeconds(textSpeed);
+
+            if (i == charArray.Length - 1)
+            {
+                // String is fully displayed, perform additional actions
+                endOfString = true;
+            }
+        }
+        
+        if (index == 3)
+        {
+            NPC.gameObject.SetActive(false);
+            arkady.gameObject.SetActive(false);
+            StartCoroutine(FadeToBlack(panel));
+            StartCoroutine(FadeToBlack(nameTagPanel));
+        }
+        else
+        {
+            Color opaquePanel = new Color(panelColor.r, panelColor.g, panelColor.b, 1f);
+            panel.GetComponent<Image>().color = opaquePanel;
+            nameTagPanel.GetComponent<Image>().color = opaquePanel;
+        }
+        
         
     }
 
