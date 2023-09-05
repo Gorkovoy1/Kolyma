fca9aa59d52981dba3a1cb9500146871c716fdb5
Vulon <83103374+VulonDev@users.noreply.github.com>
2023-09-05 16:31:55 -0700

    changed some comment wording

--- Assets/Scripts/Card Game (Elizabeth Version)/CardGameCharacter.cs
+++ Assets/Scripts/Card Game (Elizabeth Version)/CardGameCharacter.cs
@@ -5,8 +5,8 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "New Card Game Character")]
 public class CardGameCharacter : ScriptableObject
 {
-    [Header("Character Data FILL THIS OUT")]
+    [Header("Character Data")]
     public string name;
     public Sprite portrait;
-    public List<SpecialDeckCard> deckList; //the list of cards in the character's decklist AT THE START OF THE GAME. Never change this during a game.
+    public List<SpecialDeckCard> deckList; //the list of special cards in the character's decklist
 }
