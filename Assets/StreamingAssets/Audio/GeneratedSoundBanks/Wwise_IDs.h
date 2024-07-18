743d06c15b9e29da44a56adc933a491514d9c5b6
Gorkovoy1 <101889868+Gorkovoy1@users.noreply.github.com>
2024-07-17 19:39:50 -0700

    Music Added
    Music for camp added, volume settings changed

--- Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
+++ Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -20,6 +20,7 @@ namespace AK
         static const AkUniqueID PLAY_COUGH = 3234553616U;
         static const AkUniqueID PLAY_CROWS = 639442158U;
         static const AkUniqueID PLAY_DOGS_BARK = 1892391772U;
+        static const AkUniqueID PLAY_MUSIC_CAMP_DAY = 2741544670U;
         static const AkUniqueID PLAY_MUSIC_JAIL = 3643466302U;
         static const AkUniqueID PLAY_SCREEN_SFX = 1112221790U;
         static const AkUniqueID PLAY_TYPEWRITER = 57706899U;
