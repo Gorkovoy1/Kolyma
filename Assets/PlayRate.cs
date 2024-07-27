91eec8656271f9934cd0a6854bc5bd4df8814837
jazzydingo <129543893+jazzydingo@users.noreply.github.com>
2024-07-26 18:56:38 -0700

    Chimney Smoke
    added chimney smoke and changed rate of play

--- Assets/PlayRate.cs
+++ Assets/PlayRate.cs
@@ -11,7 +11,7 @@ public class PlayRate : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        speed = 0.185f;
+        
     }
 
     // Update is called once per frame
