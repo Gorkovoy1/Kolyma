e251264d77d200aee9faec7abd5d41a5412f69f0
jazzydingo <129543893+jazzydingo@users.noreply.github.com>
2025-07-01 00:21:02 -0700

    deck manager and cards for tutorial

--- Assets/2 DeckManager/DeckManagerController.cs
+++ Assets/2 DeckManager/DeckManagerController.cs
@@ -1,9 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class DeckManagerController : MonoBehaviour
 {
+
+    public TextMeshProUGUI cardCount;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +17,6 @@ public class DeckManagerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        cardCount.text = "" + CardInventoryController.instance.playerDeck.Count + " /15";
     }
 }
