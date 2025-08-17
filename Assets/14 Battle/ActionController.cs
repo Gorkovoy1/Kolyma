2ea984f4265c6be9617e92e8e8e768086eed2eb6
jazzydingo <129543893+jazzydingo@users.noreply.github.com>
2025-08-17 16:20:02 -0700

    Player Action Button
    added action of flip or swap own cards, once per round

---
+++ Assets/14 Battle/ActionController.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class ActionController : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+
+    public Button swapButton;
+    public Button flipButton;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        swapButton.gameObject.SetActive(true);
+        flipButton.gameObject.SetActive(true);
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        swapButton.gameObject.SetActive(false);
+        flipButton.gameObject.SetActive(false);
+    }
+
+    public void ActivateSwap()
+    {
+        foreach (GameObject g in NumberManager.instance.allNumbers)
+        {
+            g.GetComponent<NumberStats>().selectable = true;
+        }
+
+        CardSelectionController.instance.CallButtons("swap", "player");
+        this.gameObject.SetActive(false);
+    }
+
+    public void ActivateFlip()
+    {
+        foreach (GameObject g in NumberManager.instance.allNumbers)
+        {
+            g.GetComponent<NumberStats>().selectable = true;
+        }
+
+        CardSelectionController.instance.CallButtons("flip", "player");
+        this.gameObject.SetActive(false);
+    }
+
+}
