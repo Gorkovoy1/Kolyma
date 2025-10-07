4d4437a99c5c56cd93df8ec84e0ea8d44b7f3d14
jazzydingo <129543893+jazzydingo@users.noreply.github.com>
2025-10-07 14:24:04 -0700

    Fixed bug with dragging button to drop zone

--- Assets/14 Battle/DropZoneController.cs
+++ Assets/14 Battle/DropZoneController.cs
@@ -5,31 +5,39 @@ using UnityEngine.UI;
 public class DropZoneController : MonoBehaviour, IDropHandler
 {
     public GameObject playerDiscardZone;
+    public GameObject droppedCard;
 
     public void OnDrop(PointerEventData eventData)
     {
         Debug.Log("Card dropped into the zone!");
-
-        GameObject droppedCard = eventData.pointerDrag;
+        if(eventData.pointerDrag != null)
+        {
+            droppedCard = eventData.pointerDrag;
+        }
+        
         if (droppedCard != null)
         {
             if (!droppedCard.TryGetComponent<NumberStats>(out var component))
             {
-                if(droppedCard.GetComponent<CardPlace>().isPlayable == true)
+                if(droppedCard.TryGetComponent<CardPlace>(out var cardPlace))
                 {
-                    Debug.Log("set being played");
-                    droppedCard.GetComponent<CardPlace>().beingPlayed = true;
-                    droppedCard.GetComponent<CardPlace>().correspondingImage.GetComponent<Image>().material = droppedCard.GetComponent<CardPlace>().defaultMat;
-                    droppedCard.transform.SetParent(playerDiscardZone.transform);
+                    if (cardPlace.isPlayable == true)
+                    {
+                        Debug.Log("set being played");
+                        cardPlace.beingPlayed = true;
+                        cardPlace.correspondingImage.GetComponent<Image>().material = droppedCard.GetComponent<CardPlace>().defaultMat;
+                        droppedCard.transform.SetParent(playerDiscardZone.transform);
 
 
-                    //call script to animate card and carry out effect
-                    //droppedCard.GetComponent
+                        //call script to animate card and carry out effect
+                        //droppedCard.GetComponent
+                    }
+                    else
+                    {
+                        cardPlace.beingPlayed = false;
+                    }
                 }
-                else
-                {
-                    droppedCard.GetComponent<CardPlace>().beingPlayed = false;
-                }
+                
             }
 
         }
